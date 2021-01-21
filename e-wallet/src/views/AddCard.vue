de55a352471eb5c508004fba1991e921d82e090f
Harshada-Mulye <mulyeharshada@gmail.com>
2021-01-21 12:45:35 +0100

    Files added
    Files added

---
+++ e-wallet/src/views/AddCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+     <header>
+      <Top :title="FormTitle" />
+    </header>
+    <main>
+      </main>
+  </div>
+</template>
+<script>
+import Top from "../components/Top";
+export default {
+  components: {
+   Top
+},
+data()
+{
+  return{
+    FormTitle: "add a new card",
+  }
+}
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ e-wallet/src/views/Home.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container">
+    <header>
+      <Top :title="title"/>
+    </header>
+    <main>
+           <article class="active-card">
+        <h6>ACTIVE CARD</h6>
+        <div class="the-card">
+          <Card :card="Cards[this.position]" />
+        </div>
+      </article>
+      <article class="card-stack">
+        <CardStack
+          v-for="(card, index) in Cards"
+          :key="index"
+          :card="Cards[index]"
+          @actived="actived(index)"
+          
+         
+        />
+      </article>
+   
+      <div class="btn">
+        <router-link to="AddCard" class="add-card-btn">
+          add a new card
+        </router-link>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Top from "../components/Top";
+import Card from "../components/Card";
+import CardStack from "../components/CardStack";
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      title: "e-wallet",
+      position:0
+     
+    }
+  },
+  methods:{
+     actived(index) {
+         this.position = index;
+       console.log("hi");
+     
+  }
+  },
+  components: {
+    Top,
+    Card,
+    CardStack
+  },
+  computed: {
+    Cards() {
+      return this.$root.$data.cards;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  main {
+    max-width: 375px;
+    margin: auto;
+    .active-card {
+      h6 {
+        margin: 0 0 5px 0;
+      }
+    }
+    
+  .the-card{
+    margin-bottom: 50px;
+  }
+  .btn {
+      display: flex;
+      width: 350px;
+      margin: auto;
+      margin-top:20px;
+      border-radius: 5px;
+      justify-content: center;
+      text-transform: uppercase;
+      border: 1px solid black;
+      .add-card-btn {
+        width: 100%;
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+        padding: 10px 20px;
+     
+      }
+      .add-card-btn:hover {
+        background-color: #000;
+        border: none;
+        color: aliceblue;
+        transition: 200ms;
+        cursor: pointer;
+      }
+}
+}
+}
+</style>
