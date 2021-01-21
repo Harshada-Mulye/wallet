217c82016c44d73a76de72ebd63f08de60c709e3
Harshada-Mulye <mulyeharshada@gmail.com>
2021-01-21 13:20:09 +0100

    add-form added

--- e-wallet/src/views/AddCard.vue
+++ e-wallet/src/views/AddCard.vue
@@ -4,20 +4,51 @@
       <Top :title="FormTitle" />
     </header>
     <main>
+      <section class="default-card">
+      <card :card="DefaultCard"/>
+        </section>
+        <section class="add-card-form">
+          <form class="submit-card" @submit.prevent="send" ref="form">
+            <label for="cardNumber">Card Number</label>
+            <the-mask  mask="#### #### #### ####"
+            type="text"
+            name="cardNumber"
+            class="card-number"
+            placeholder="XXXXXXXXXXXXXXXX"/>
+            </form>
+
+          </section>
       </main>
   </div>
 </template>
 <script>
 import Top from "../components/Top";
+import Card from "../components/Card";
 export default {
   components: {
-   Top
+   Top,Card
 },
 data()
 {
   return{
     FormTitle: "add a new card",
+     DefaultCard: {
+        id: "432984728397",
+        holder: "firstname lastname",
+        vendor: "bitcoin",
+        number: "XXXX XXXX XXXX XXXX",
+        validMonth: "MM",
+        validYear: "YY",
+        bgColor: "#D8D8D8",
+        textColor: "#2c3e50",
+        img: "bitcoin",
+      },
   }
+},
+methods:{
+send(){
+  this.DefaultCard.number=this.$refs.form.cardNumber.value;
+}
 }
 }
 </script>
